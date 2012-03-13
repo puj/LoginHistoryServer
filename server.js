b91b9b9a2d52cab0af0b82d5cbce30e0bc255b2b
puj <vango85@gmail.com>
2012-03-14 00:50:33 +0100

    Updated readme with prereqs and more output on server for checking POST

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(require('connect').bodyDecoder());
 
 app.post('/', function(req, res){
   req.accepts('application/json');
-  console.log(req.body);
+  console.log(req.body.username);
+  console.log(req.body.password.toString("utf8"));
   res.send('Hello World');
 });
 
