3a0ded1c991a65a846a3a6d5ed5b6da95c540241
puj <vango85@gmail.com>
2012-03-13 20:38:50 +0100

    Simple node server to test android connectivity

---
+++ server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  console.log(req);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(80, '127.0.0.1');
