03be1b4277f3f1abf0e9c9259a9e1a2973c10bb5
puj <vango85@gmail.com>
2012-03-15 00:04:40 +0100

    Register hook is alive and checks for duplicates.  Mongo hooks working

--- public/site.js
+++ public/site.js
@@ -1,16 +1,31 @@
+// Returns the SHA512 hash from the str
 function getSHA512(str){
 	var shaObj = new jsSHA(str, "ASCII");
 	var hash = shaObj.getHash("SHA-512", "HEX");
 	return hash;
 }
 
+// Uses the user input credentials to register the user
 function login(){
+	// Create password hash input from username, password, and salt
 	var username = document.getElementById('txtUsername').value;
 	var password = document.getElementById('txtPassword').value;
 	var salt = '09234j234kj!@#213lk$#$)(*)DFSDFL##$';
 	var passwordAndSalt = username + password + salt;
 
-
+	// Get a SHA512 hash from the credentials
 	var hash = getSHA512(passwordAndSalt);
-	alert(username + " : " + hash);
+	
+	// Create a simple JSON object to post
+	var userObj = {
+		username : username,
+		pwHash : passwordAndSalt,
+	}
+
+	// Post the registration event to the server
+	$.post("/register", userObj,
+	  function(data){
+	  	alert("post success");
+	  }
+	);
 }
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express.createServer();
 var jade = require('jade');
-  
+var LoginProvider = require('./loginprovider').LoginProvider;
+var loginProvider = new LoginProvider('localhost', 27017);
+
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -11,15 +13,45 @@ app.configure(function(){
     app.use("/public", express.staticProvider(__dirname + '/public'));
 });
 
-	
+
 
 app.get('/',function(req,res){
   	res.render('login', { }, function(err,data){
   		res.end(data);
   	});
-	
+
+	loginProvider.registerUser("puj","lksdf",function(err,data){
+		console.log(err + " : " + JSON.stringify(data));
+	});
 
 });
+
+
+app.post('/register',function(req, res){
+	req.accepts('application/json');
+
+  // Get the credentials from the request
+  var username = req.body.username;
+  var pwHash = req.body.pwHash;
+
+  // First check if the user already exists
+  loginProvider.getUserByUsername(username, function(err,data){
+    if(err){
+      // This user does not already exist
+      // Create the user in the DB instance
+      loginProvider.registerUser(username,pwHash, function(err, data){
+        if(err){
+          res.send({result: 'failure', data : "DB error"});
+        }else{
+          res.send({result: 'success', data : data});  
+        }
+      });  
+    }else{
+      // Use already exists, cannot create.
+      res.send({result : 'failure', data : "User already exists"})
+    }
+  });
+});	
 
 app.post('/', function(req, res){
   req.accepts('application/json');
